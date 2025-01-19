77b97be3be8bec4afcc1fbfc85dfe71b836e63e8
krrish73200 <44476712+krrish73200@users.noreply.github.com>
2025-01-19 23:24:36 +0530

    welcome comment change for learning purpose

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Welcome to Inventory Management System!"
+    return "Welcome to my first Inventory Management System! "
 
 if __name__ == '__main__':
     app.run(debug=True)
